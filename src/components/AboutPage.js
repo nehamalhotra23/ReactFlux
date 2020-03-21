263c0ade13c6246be8ef35e8ae3c83995236dc80
Neha Malhotra <nehamalhotra@Nehas-MacBook.local>
2020-03-21 16:26:16 -0700

    add react components

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+        <p>This app uses React</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
