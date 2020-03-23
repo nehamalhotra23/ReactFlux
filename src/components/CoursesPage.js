66586d56c78eec1cef51edd8224ad1f4c0cc4c42
Neha Malhotra <nehamalhotra@Nehas-MacBook.local>
2020-03-23 15:16:01 -0700

    add hooks on coursespage

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,39 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: []
-  };
-  componentDidMount() {
-    getCourses().then(courses => this.setState({ courses: courses }));
-  }
-  renderRow(course) {
-    return (
-      <tr>
-        <td>{course.title}</td>
-        <td>{course.authorId}</td>
-        <td>{course.category}</td>
-      </tr>
-    );
-  }
-  render() {
-    return (
-      <>
-        <h1>Courses Page</h1>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author Id</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>{this.state.courses.map(this.renderRow)}</tbody>
-        </table>
-      </>
-    );
-  }
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  });
+
+  return (
+    <>
+      <h1>Courses Page</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author Id</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map(course => {
+            return (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>{course.authorId}</td>
+                <td>{course.category}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursesPage;
